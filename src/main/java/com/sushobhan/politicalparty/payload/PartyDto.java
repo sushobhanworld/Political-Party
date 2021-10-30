37cd5889ba70e4d4f2bbde1d5379fc7d9d15388b
Anjana <anjanamaiti15@gmail.com>
2021-10-31 01:00:36 +0530

    Adding the updated files

--- src/main/java/com/sushobhan/politicalparty/payload/PartyDto.java
+++ src/main/java/com/sushobhan/politicalparty/payload/PartyDto.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PartyDto {
+
     private Long id;
     @NotEmpty(message = "PartyName field is required")
     @Size(min = 5, max = 100, message = "Party name should be at least 5 characters and max 100 characters")
@@ -21,4 +22,10 @@ public class PartyDto {
     private String founderName;
     @NotEmpty(message = "FoundationYear field is required")
     private Double foundationYear;
+
+    public PartyDto(String partyName, String founderName, Double foundationYear) {
+        this.partyName = partyName;
+        this.founderName = founderName;
+        this.foundationYear = foundationYear;
+    }
 }
--- src/test/java/com/sushobhan/politicalparty/controller/PartyControllerTest.java
+++ src/test/java/com/sushobhan/politicalparty/controller/PartyControllerTest.java
@@ -2,26 +2,29 @@ package com.sushobhan.politicalparty.controller;
 
 import com.sushobhan.politicalparty.entity.PartyEntity;
 import com.sushobhan.politicalparty.payload.PartyDto;
-import com.sushobhan.politicalparty.payload.PartyResponse;
 import com.sushobhan.politicalparty.service.PartyService;
-import com.sushobhan.politicalparty.utlils.Mapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.testcontainers.shaded.org.hamcrest.Matchers;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 
 @WebMvcTest(PartyController.class)
 class PartyControllerTest {
@@ -37,7 +40,26 @@ class PartyControllerTest {
     }
 
     @Test
-    void createPoliticalParty() {
+    void createPoliticalParty() throws Exception {
+        String json = "{\n" +
+                "    \"partyName\": \"ABC India party\",\n" +
+                "    \"founderName\": \"Kumar1\",\n" +
+                "    \"foundationYear\": 1995\n" +
+                "}";
+        PartyDto partyDto = new PartyDto("ABC India party", "Kumar1", 1995D);
+
+        Mockito.when(partyService.createParty(ArgumentMatchers.any(PartyDto.class))).thenReturn(partyDto);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/politics/api/v1/addParty")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult mvcResult = this.mockMvc.perform(requestBuilder).andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
 
     }
 
